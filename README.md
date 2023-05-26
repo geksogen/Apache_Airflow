f237909f6461485c6358e4c0fa63f471b574e0e0
Your Name <you@example.com>
2023-05-26 05:42:36 +0000

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 ## Data Integration (Apache Airflow + Python)
+
+### Apache Airflow install
+
+```bash
+apt install python3-pip
+export AIRFLOW_HOME=~/airflow
+pip3 install "apache-airflow==2.6.1" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.6.1/constraints-3.10.txt"
+airflow standalone
+```
+### Install Python packages
+
+```bash
+pip3 install pandas
+pip3 install db-sqlite3
+pip3 install xmltodict
+pip3 install requests
+mkdir /root/airflow/dags
+```
---
+++ main.py
@@ -0,0 +1,28 @@
+import xmltodict
+import requests
+
+from airflow.decorators import dag, task
+import pendulum
+
+PODCAST_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
+
+@dag(
+  dag_id='podcast_summary',
+  schedule_interval="@daily",
+  start_date=pendulum.datetime(2023, 3, 25),
+  catchup=False,
+)
+
+def podcast_summary():
+  @task()
+    def get_episodes():
+        data = requests.get(PODCAST_URL)
+        feed = xmltodict.parse(data.text)
+        episodes = feed["rss"]["channel"]["item"]
+        print(f"Found {len(episodes)} episodes.")
+        return episodes
+
+    podcast_episodes = get_episodes()
+
+
+summary = podcast_summary()
