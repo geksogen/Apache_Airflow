565f99b5db88689bc47ae6d25883b6d8d6f7c43c
Andrey.Torshin <dchorch@gmail.com>
2023-06-07 13:33:15 +0300

    Create dag_audio_text.py

---
+++ dag_audio_text.py
@@ -0,0 +1,42 @@
+import xmltodict
+import requests
+import os
+import json
+
+from airflow.decorators import dag, task
+import pendulum
+
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.sqlite.hooks.sqlite import SqliteHook
+
+from pydub import AudioSegment
+
+@dag(
+  dag_id='podcast_audio_text',
+  schedule="@daily",
+  start_date=pendulum.datetime(2023, 3, 25),
+  catchup=False,
+)
+
+def audio_text():
+    @task()
+    def resize_audio():
+      startMin = 0
+      startSec = 0
+      endMin = 0
+      endSec = 60
+
+      # Time to miliseconds
+      startTime = startMin * 60 * 1000 + startSec * 1000
+      endTime = endMin * 60 * 1000 + endSec * 1000
+
+      # Opening file and extracting segment
+      song = AudioSegment.from_mp3('./episodes/soung.mp3')
+      extract = song[startTime:endTime]
+
+      ## Saving extract
+      extract.export('./episodes/extract.mp3', format="mp3")
+      
+    resize_audio()
+    
+rezult = audio_text()
