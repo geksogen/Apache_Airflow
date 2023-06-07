12f761c487869117cb9af210747cde3c3a317bed
Andrey.Torshin <dchorch@gmail.com>
2023-06-07 15:44:50 +0300

    Update dag_audio_text.py

--- dag_audio_text.py
+++ dag_audio_text.py
@@ -11,21 +11,25 @@ from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 
 from pydub import AudioSegment
 
+PODCAST_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
+
 @dag(
-  dag_id='podcast_audio_text',
+  dag_id='podcast',
   schedule="@daily",
-  start_date=pendulum.datetime(2023, 5, 25),
+  start_date=pendulum.datetime(2023, 3, 25),
   catchup=False,
 )
 
 def podcast_summary():
     @task()
     def get_episodes():
-        data = 'test'
-        return data
+        data = requests.get(PODCAST_URL)
+        feed = xmltodict.parse(data.text)
 
-    test = get_episodes()
-    print(test)
+        episodes = feed["rss"]["channel"]["item"]
+        print(f"Found {len(episodes)} episodes. @task get_episodes **OK**")
+        return episodes
+
+    podcast_episodes = get_episodes()
 
 summary = podcast_summary()
-print("hello")
