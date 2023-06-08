3a75cc6f6c17b640720bae21462877ff2d5ddb89
Andrey.Torshin <dchorch@gmail.com>
2023-06-08 10:36:40 +0300

    Update dag_audio_text.py

--- dag_audio_text.py
+++ dag_audio_text.py
@@ -12,6 +12,8 @@ from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 from pydub import AudioSegment
 from vosk import Model, KaldiRecognizer
 
+import librosa
+
 @dag(
   dag_id='podcast',
   schedule="@daily",
@@ -24,15 +26,11 @@ def audio_to_text():
     def resize_audio():
         
         FRAME_RATE = 16000
-
-        model = Model(model_name="vosk-model-small-en-us-0.15")
-        rec = KaldiRecognizer(model, FRAME_RATE)
-        rec.SetWords(True)
         
         filepath = os.path.join('./episodes', 'sound.mp3')
-        mp3 = AudioSegment.from_mp3(filepath)
-        mp3 = mp3.set_channels(1)
-        mp3 = mp3.set_frame_rate(FRAME_RATE)
+        mp3 = librosa.util.example_audio_file(filepath)
+        y1, sr1 = librosa.load(mp3, sr=16000)
+        print(sr1)
         
         print(f"Обработка завершена. **OK**")
 
